12476f19dcf79e0a376759e41005d994ea99ee40
David Kassin <david.j.kassin@gmail.com>
2022-06-06 18:10:38 -0600

    Add unshelfbook function

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,26 @@
 
+
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(book_name, shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book_name) {
+      shelf.splice(i,1)
+    }
+  }
+}
+
+
+
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
