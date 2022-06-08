ddbd1c1597fa6557c9f4c121836d26cad39fdf78
David Kassin <david.j.kassin@gmail.com>
2022-06-08 07:07:31 -0600

    Add all working code for Library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,36 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return(library);
+}
 
+function addBook(library, book) {
+  // var genre = book.genre;
+  // var shelf = library.shelves[genre];
+  // shelf.push(book)
+  // }
+  // Combines the three lines into one
+  library.shelves[book.genre].push(book)
+}
+
+function checkoutBook(library, book_title, genre) {
+  for (i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === book_title) {
+      library.shelves[genre].splice(i,1)
+      return `You have now checked out ${book_title} from the ${library.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${book_title} available at the ${library.name}`
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
